fedded3dc2b35989729e6ddea7dfdbeef2875496
kgashok <lifebalance@gmail.com>
2018-04-10 17:58:43 +0000

    Code to differentiate malloc and calloc

---
+++ code/calloc-overflow-demo.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdint.h>
+
+// Thanks to https://vorpus.org/blog/why-does-calloc-exist/
+// Also read http://j.mp/cs2851-dynamicMemory
+//
+int main(int argc, char** argv)
+{
+    size_t huge = INTPTR_MAX;
+
+    void* buf = malloc(huge * huge);
+    if (!buf) perror("malloc failed");
+    printf("malloc(huge * huge) returned: %p\n", buf);
+    free(buf);
+
+    buf = calloc(huge, huge);
+    if (!buf) perror("calloc failed");
+    printf("calloc(huge, huge) returned: %p\n", buf);
+    free(buf);
+}
