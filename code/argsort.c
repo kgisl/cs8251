ceaa18ba1a3ef48f973f72a23027dcefb3ee8df8
Ashok Bakthavathsalam <kgashok@users.noreply.github.com>
2020-03-16 10:46:55 +0530

    Update argsort.c

--- code/argsort.c
+++ code/argsort.c
@@ -1,48 +1,51 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-int comparator(const void *a, const void *b) {
-  const char *pa = *(const char **)a;
-  const char *pb = *(const char **)b;
-  printf("%s %s\n", pa, pb);
-  return strcmp(pa, pb) < 0;
-
-  // Cast 'a' to a pointer to a constant pointer to a character and dereference
-  // that
-  // Credits: https://bewuethr.github.io/2015-03-07-sorting-strings-in-c-with-qsort.html
-  // return strcmp(*(char* const*) a, *(char* const*) b);
-}
-
-int comparator_char(const void *a, const void *b) {
-  printf("%c %c\n", *(char *)a, *(char *)b);
-  return (*(char *)a - *(char *)b);
-}
-
 /*
  * program sorts command line arguments in ascending order
  *   - if only 1 argument provided, the characters are sorted
  *   - sorting is accomplished using qsort and
  *   - and two comparators
  *
- *   Usage: ./a.out abcdzyxghi -> abcdghixyz
- *          ./a.out argon zero black -> argon black zero
+ *   Usage: ./a.out zyxghiabcd -> abcdghixyz
+ *          ./a.out zero black argon -> argon black zero
  *
  */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int compare_strings(const void *a, const void *b) {
+  const char *pa = *(const char **)a;
+  const char *pb = *(const char **)b;
+  printf("%s %s\n", pa, pb);
+  return strcmp(pa, pb);
+
+  // Cast 'a' to a pointer to a constant pointer
+  // to a character and dereference that
+  // Credits:
+  // https://bewuethr.github.io/2015-03-07-sorting-strings-in-c-with-qsort.html
+  // return strcmp(*(char* const*) a, *(char* const*) b);
+}
+
+int compare_chars(const void *a, const void *b) {
+  printf("%c %c\n", *(char *)a, *(char *)b);
+  return (*(char *)a - *(char *)b);
+}
+
 int main(int argc, char *argv[]) {
+  if (argc == 1) {
+    puts("No args to sort!\n");
+    exit(0);
+  }
   if (argc == 2) {
-    qsort(argv[1], strlen(argv[1]), sizeof(char), comparator_char);
+    qsort(argv[1], strlen(argv[1]), sizeof(char), compare_chars);
     puts("After sorting letters in argument 1...");
     printf("%s\n", argv[1]);
-  } else if (argc > 2) {
-    qsort(&argv[1], argc - 1, sizeof(char *), comparator);
+  }
+  if (argc > 2) {
+    qsort(&argv[1], argc - 1, sizeof(char *), compare_strings);
     puts("After sorting command line arguments..");
-    while (argc > 1)
-      printf("%s\n", argv[--argc]);
-  } else {
-    puts("No args to sort!");
+    for (int i = 1; i < argc; i++)
+      printf("%s\n", argv[i]);
   }
 
   return 0;
 }
-
